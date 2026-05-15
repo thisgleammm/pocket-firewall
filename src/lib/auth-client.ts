f24f7c7e4cb4451f5296a3eeb64de0abb411161c
thisgleammm <thisgleam@gmail.com>
2026-05-15 11:40:29 +0700

    fix 404

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,9 +1,5 @@
 import { createAuthClient } from "better-auth/react"
-import { sentinelClient } from "@better-auth/infra/client";
 
 export const authClient = createAuthClient({
-    baseURL: process.env.NEXT_PUBLIC_APP_URL!,
-    plugins: [
-        sentinelClient()
-    ]
+    baseURL: process.env.NEXT_PUBLIC_APP_URL!
 })
