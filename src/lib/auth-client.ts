ef17d1d6e4efb28acec1f82b62a2bdfc1f02b57a
thisgleammm <thisgleam@gmail.com>
2026-05-15 11:09:50 +0700

    update infra betterauth

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,9 @@
 import { createAuthClient } from "better-auth/react"
+import { sentinelClient } from "@better-auth/infra/client";
 
 export const authClient = createAuthClient({
-    baseURL: process.env.NEXT_PUBLIC_APP_URL!
+    baseURL: process.env.NEXT_PUBLIC_APP_URL!,
+    plugins: [
+        sentinelClient()
+    ]
 })
